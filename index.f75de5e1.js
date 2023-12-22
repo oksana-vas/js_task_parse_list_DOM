7e1cb7cdb5eca8ab0c7bd39c37f4239c338d86fd
Oksana Vasyliuk <oksana.vasyliuk13@gmail.com>
2023-12-22 13:27:45 +0100

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,31 @@
+"use strict";
+const listElement = document.querySelector("ul");
+const employees = [
+    ...document.querySelectorAll("li")
+];
+function sortList(list) {
+    const sorted = [
+        ...list
+    ].sort((item1, item2)=>{
+        return convertSalary(item2.dataset.salary) - convertSalary(item1.dataset.salary);
+    });
+    sorted.forEach((employee)=>listElement.append(employee));
+}
+function getEmployees(list) {
+    return list.map((item)=>{
+        const { position, salary, age } = item.dataset;
+        return {
+            name: item.textContent,
+            position,
+            salary,
+            age
+        };
+    });
+}
+function convertSalary(salary) {
+    return +salary.slice(1).replaceAll(",", "");
+}
+sortList(employees);
+getEmployees(employees);
+
+//# sourceMappingURL=index.f75de5e1.js.map
